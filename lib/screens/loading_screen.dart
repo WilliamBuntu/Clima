b789cfd5a8f188ea6f68c6689660ab22ebab3f08
WilliamBuntu <williambuntu1@gmail.com>
2024-07-11 19:46:14 +0200

    Clima app

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+// import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+            // getLocation();
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
