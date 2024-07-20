77e44177113d4ef92d9d2cd468782c74e434d25d
WilliamBuntu <williambuntu1@gmail.com>
2024-07-21 00:19:16 +0200

    changed baground picture

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -54,7 +54,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: const AssetImage('images/location_background.jpg'),
+            image: const AssetImage('images/nn.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
